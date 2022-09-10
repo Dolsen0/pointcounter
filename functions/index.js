670c7e7f47cf5df383b7cd8f1aa4dd412e26b948
dolsen0 <derek.olsen@gmail.com>
2022-09-10 01:04:47 -0400

    started api

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+import functions from 'firebase-functions'
+import express from 'express';
+import cors from 'cors'
+import { getMain, createUser } from './src/functions.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/pointcounter", getMain)
+app.get("/pointcounter/signup", createUser)
+
+app.post("/pointcounter/signup", createUser)
+
+
+
+export const api = functions.https.onRequest(app)
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ functions/src/functions.js
@@ -0,0 +1,12 @@
+export function getMain(req, res) {
+  return (
+
+    res.send("hello world")
+    )
+    ;
+}
+
+export function createUser(req, res) {
+  return (res.send("hello there"));
+}
+
