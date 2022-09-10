04e79467cc75f36001332ca7c2f35794aa35486c
dolsen0 <derek.olsen@gmail.com>
2022-09-10 13:19:48 -0400

    connected api to db and created getUser

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,14 @@
 import functions from 'firebase-functions'
 import express from 'express';
 import cors from 'cors'
-import { getMain, createUser } from './src/functions.js';
+import { getMain, createUser, getUser } from './src/functions.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/pointcounter", getMain)
+app.get("/pointcounter/frank", getUser)
 app.get("/pointcounter/signup", createUser)
 
 app.post("/pointcounter/signup", createUser)
--- functions/src/functions.js
+++ functions/src/functions.js
@@ -1,6 +1,10 @@
+import { user } from 'firebase-functions/v1/auth';
+import dbConnect from './dbConnect.js';
+
+
 export function getMain(req, res) {
   return (
-
+    
     res.send("hello world")
     )
     ;
@@ -10,3 +14,17 @@ export function createUser(req, res) {
   return (res.send("hello there"));
 }
 
+export async function  getUser(req, res){
+
+    const db = dbConnect();
+    const collection = await db.collection('users').get()
+        .catch(err => res.status(500).send(err))
+    const users = collection.docs.map( doc => {
+        let user = doc.data();
+        user.id = doc.id;
+        return user
+    })   
+    return(
+        res.send(users[0]) 
+    )
+}
